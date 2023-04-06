c61064e18abc8f640456e3ceb31d20dae658dfbf
Kevin Bernfeld <kcbernfeld@gmail.com>
2023-04-06 14:57:08 -0400

    Initial Commit

---
+++ vaporwave/vaporwaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  vaporwaveApp.swift
+//  vaporwave
+//
+//  Created by Kevin Bernfeld on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct vaporwaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
